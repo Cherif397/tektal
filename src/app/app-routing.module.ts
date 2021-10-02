f28ed4f97b369015ea669559d9b3a78dcaddf03d
Cherif397 <79149952+Cherif397@users.noreply.github.com>
2021-10-02 13:24:09 +0200

    site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './nutrition/home/home.component';
+import { LandingPageNutritionComponent } from './nutrition/landing-page-nutrition/landing-page-nutrition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
